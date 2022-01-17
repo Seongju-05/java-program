58302bc43f304fdfe4f83ab1f19e3f7c381b4bda
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-01-17 13:59:28 +0900

    Add files via upload

---
+++ eclipse-workspace/Chapter6/src/thisex/CallAnotherConst.java
@@ -0,0 +1,31 @@
+package thisex;
+
+class Person {
+	String name;
+	int age;
+	
+	public Person() {
+		
+		this("이름 없음", 1);
+	}
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person returnSelf() {
+		return this;
+	}
+	
+}
+public class CallAnotherConst {
+
+	public static void main(String[] args) {
+
+		Person p1 = new Person();	
+		System.out.println(p1.name);
+		
+		System.out.println(p1.returnSelf());
+	}
+}
