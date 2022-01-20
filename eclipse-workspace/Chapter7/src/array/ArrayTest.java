e6030c1493c7d61fcf5ddef1fd7acc56ec9338f3
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-01-20 14:03:07 +0900

    Add files via upload

---
+++ eclipse-workspace/Chapter7/src/array/ArrayTest.java
@@ -0,0 +1,21 @@
+package array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+
+		
+		int[] numbers = new int [3];
+		
+		numbers[0] = 1;
+		numbers[1] = 2;
+		numbers[2] = 3;
+		
+		for (int i = 0; i< numbers.length; i++)
+		{
+			System.out.println(numbers[i]);
+		}
+		
+	}
+
+}
---
+++ eclipse-workspace/Chapter7/src/array/ArrayTest3.java
@@ -0,0 +1,19 @@
+package array;
+
+public class ArrayTest3 {
+
+	public static void main(String[] args) {
+
+		char[] alphabets = new char[26];
+		char ch = 'A';
+		
+		for (int i = 0; i < alphabets.length; i++, ch++) {
+			alphabets[i] = ch;
+		}
+		
+		for (int i = 0; i < alphabets.length; i++, ch++) {
+			System.out.println(alphabets[i]);
+		}
+	}
+
+}
