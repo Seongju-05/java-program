bddd3ab80f41c84c0d9562d9500dc282634479f7
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-01-26 16:01:00 +0900

    Add files via upload

---
+++ eclipse-workspace/Test/src/test/ArrayListTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("aaa");
+		list.add("bbb");
+		list.add("ccc");
+		
+		for(int i = 0; i< list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+
+}
