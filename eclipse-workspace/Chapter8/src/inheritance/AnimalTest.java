bd3ea1158cdf71c0f13b6d85cbef3c5ce6600809
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-02-05 22:05:36 +0900

    Add files via upload

---
+++ eclipse-workspace/Chapter8/src/inheritance/AnimalTest.java
@@ -0,0 +1,51 @@
+package inheritance;
+
+import java.util.ArrayList;
+
+class Animal{
+	public void move() {
+		System.out.println("동물이 움직입니다.");
+	}
+}
+
+class Human extends Animal{
+	public void move() {
+		System.out.println("사람이 두발로 걷습니다.");
+	}
+}
+
+class Tiger extends Animal{
+	public void move() {
+		System.out.println("호랑이가 네발로 뜁니다.");
+	}
+}
+
+class Eagle extends Animal {
+	public void move() {
+		System.out.println("독수리가 하늘을 날읍니다.");
+	}
+}
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		
+		AnimalTest test = new AnimalTest();
+		test.moveAnimal(new Human());
+		test.moveAnimal(new Tiger());
+		test.moveAnimal(new Eagle());
+		
+		//Animal animal = new Human();
+		
+		Animal[] animalList = new Animal[3];
+		animalList[0] = new Human();
+		
+		ArrayList<Animal> animals = new ArrayList<Animal>();
+		animals.add(new Tiger());
+	}
+	
+	public void moveAnimal( Animal animal    ) {
+		animal.move();
+
+	}
+}
