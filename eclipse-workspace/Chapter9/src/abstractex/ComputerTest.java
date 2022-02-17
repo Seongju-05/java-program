19b75e5b327e968d0df36023cf040eae6b9ed1f6
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-02-17 15:24:26 +0900

    Add files via upload

---
+++ eclipse-workspace/Chapter9/src/abstractex/ComputerTest.java
@@ -0,0 +1,15 @@
+package abstractex;
+
+public class ComputerTest {
+	public static void main (String[] args) {
+		
+		//Computer c1 = new Computer();
+		Computer c2 = new DeskTop();
+		//Computer c3 = new NoteBook();
+		
+		Computer c4 = new MyNoteBook();
+		
+		c4.display();
+		c2.display();
+	}
+}
