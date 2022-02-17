c41a94159057284489df94a106da411499d9996c
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-02-17 15:23:57 +0900

    Add files via upload

--- eclipse-workspace/Chapter9/src/abstractex/Computer.java
+++ eclipse-workspace/Chapter9/src/abstractex/Computer.java
@@ -1,5 +1,15 @@
 package abstractex;
 
-public class Computer {
+public abstract class Computer {
 
+	public abstract void display() ;
+	public abstract void typing();
+	
+	public void turnOn() {
+		System.out.println("전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("전원을 끕니다.");
+	}
 }
