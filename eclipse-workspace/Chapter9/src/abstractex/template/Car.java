defac235cf1dccf82007e9dadb88ac7421bd4c27
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-02-21 16:47:51 +0900

    Add files via upload

--- eclipse-workspace/Chapter9/src/abstractex/template/Car.java
+++ eclipse-workspace/Chapter9/src/abstractex/template/Car.java
@@ -1,5 +1,27 @@
 package template;
 
-public class Car {
+public abstract class Car {
 
+	public abstract void drive();
+	public abstract void stop();
+	
+	public abstract void wiper();
+	public void washCar() { };
+	
+	public void startCar() {
+		System.out.println("시동을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("시동을 끕니다.");
+	}
+	
+	public final void run() {
+		startCar();
+		drive();
+		stop();
+		wiper();
+		washCar();
+		turnOff();
+	}
 }
