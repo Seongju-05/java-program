6be8e777adc89da0ef094ff6b678f8a00e9c2382
김성주 <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-04-06 09:58:06 +0900

    Add files via upload

---
+++ eclipse web/java2_1/src/kr/hs/dgsw/java/dept1/d0406/Other.java
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.java.dept1.d0406;
+
+public class Other {
+	
+	public static void main(String[] args) {
+		AccessModifier accessModifier = new AccessModifier();
+		accessModifier.publicValue = 5;
+		accessModifier.privateValue = 7;
+		accessModifier.protectedValue = 3;
+		accessModifier.defaulValue = 1;	
+	}
+}
